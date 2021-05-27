7dc6d1eff74f8319bbf14237ca6348720589b127
dante <dantecherng@hotmail.com.tw>
2021-05-28 01:19:28 +0800

    Add edit spec for mission (#6)

--- spec/feature/mission_spec.rb
+++ spec/feature/mission_spec.rb
@@ -51,4 +51,29 @@ RSpec.describe 'Visit mission path', type: :feature do
       is_expected.to have_button('建立任務')
     end
   end
+  
+  context 'when goes to page for editing missions' do
+    before do
+      visit edit_mission_path(Mission.find_by(title: new_mission.title))
+    end
+    
+    it 'has label' do
+      is_expected.to have_content('任務修改')
+      is_expected.to have_content('Title')
+      is_expected.to have_content('Content')
+      is_expected.to have_content('Started at')
+      is_expected.to have_content('Ended at')
+    end
+
+    it 'has text_field' do
+      is_expected.to have_field(id: 'mission_title', with: "#{new_mission.title}")
+      is_expected.to have_field(id: 'mission_content', with: "#{new_mission.content}")
+      is_expected.to have_field(id: 'mission_started_at', with: "#{new_mission.started_at.strftime('%Y-%m-%dT%H:%M:%S')}")
+      is_expected.to have_field(id: 'mission_ended_at', with: "#{new_mission.ended_at.strftime('%Y-%m-%dT%H:%M:%S')}")
+    end
+
+    it 'has button' do
+      is_expected.to have_button('修改任務')
+    end
+  end  
 end
