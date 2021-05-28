8214e1a40efa39d5b3eb157e4cb2c511a177f9db
dante <dantecherng@hotmail.com.tw>
2021-05-28 23:11:10 +0800

    Fix spec for dirction

--- spec/feature/mission_spec.rb
+++ spec/feature/mission_spec.rb
@@ -1,16 +1,50 @@
 require 'rails_helper'
+Capybara.current_driver = :rack_test
 
-RSpec.describe 'Visit mission path', type: :feature do
+RSpec.describe 'Missions List', type: :feature do
   subject { page }
+  
+  def find_mission(mission)
+    Mission.find_by(title: mission.title).id
+  end
 
+  let!(:mission) { FactoryBot.create(:mission) }
   let!(:new_mission) { FactoryBot.create(:mission) }
-   
-  context 'when goes to page for showing missions' do
+  let!(:updated_mission) { FactoryBot.create(:mission) }
+
+  before do
+    visit missions_path
+  end
+ 
+  it do
+    is_expected.to have_content('任務列表')
+    is_expected.to have_content('新增任務')
+  end
+  
+  context 'when there are missions' do
+    it 'shows the missions' do
+      is_expected.to have_content('任務列表')
+      is_expected.to have_content('新增任務')
+      is_expected.to have_content('修改任務')
+      is_expected.to have_content('刪除任務')
+      is_expected.to have_content("#{mission.title}")
+      is_expected.to have_content("#{mission.content}")
+      is_expected.to have_content("#{mission.started_at.strftime('%Y/%m/%d %H:%M')}")
+      is_expected.to have_content("#{mission.ended_at.strftime('%Y/%m/%d %H:%M')}")
+    end
+  end
+  
+  describe 'Adding missions' do
     before do
-      visit missions_path
+      visit new_mission_path
+      fill_in 'Title', with: new_mission.title
+      fill_in 'Content', with: new_mission.content
+      fill_in 'Started at', with: new_mission.started_at
+      fill_in 'Ended at', with: new_mission.ended_at
+      click_button '建立任務'
     end
 
-    it 'has mission' do
+    it 'adds missions' do
       is_expected.to have_content('任務列表')
       is_expected.to have_content('新增任務')
       is_expected.to have_content('修改任務')
@@ -20,60 +54,41 @@ RSpec.describe 'Visit mission path', type: :feature do
       is_expected.to have_content("#{new_mission.started_at.strftime('%Y/%m/%d %H:%M')}")
       is_expected.to have_content("#{new_mission.ended_at.strftime('%Y/%m/%d %H:%M')}")
     end
+  end
 
-    it 'has no mission' do
+  describe 'Updating missions' do
+    before do
+      visit edit_mission_path(find_mission(new_mission))
+      fill_in 'Title', with: updated_mission.title
+      fill_in 'Content', with: updated_mission.content
+      fill_in 'Started at', with: updated_mission.started_at
+      fill_in 'Ended at', with: updated_mission.ended_at
+      click_button '修改任務'
+    end
+
+    it 'updates missions' do
       is_expected.to have_content('任務列表')
       is_expected.to have_content('新增任務')
-    end
-  end  
-
-  context 'when goes to page for creating missions' do
-    before do
-      visit new_mission_path
-    end
-    
-    it 'has label' do
-      is_expected.to have_content('任務新增')
-      is_expected.to have_content('Title')
-      is_expected.to have_content('Content')
-      is_expected.to have_content('Started at')
-      is_expected.to have_content('Ended at')
-    end
-
-    it 'has text_field' do
-      is_expected.to have_field(id: 'mission_title')
-      is_expected.to have_field(id: 'mission_content')
-      is_expected.to have_field(id: 'mission_started_at')
-      is_expected.to have_field(id: 'mission_ended_at')
-    end
-
-    it 'has button' do
-      is_expected.to have_button('建立任務')
+      is_expected.to have_content('修改任務')
+      is_expected.to have_content('刪除任務')
+      is_expected.to have_content("#{updated_mission.title}")
+      is_expected.to have_content("#{updated_mission.content}")
+      is_expected.to have_content("#{updated_mission.started_at.strftime('%Y/%m/%d %H:%M')}")
+      is_expected.to have_content("#{updated_mission.ended_at.strftime('%Y/%m/%d %H:%M')}")
     end
   end
-  
-  context 'when goes to page for editing missions' do
+
+  describe 'Deleting missions' do
     before do
-      visit edit_mission_path(Mission.find_by(title: new_mission.title))
-    end
-    
-    it 'has label' do
-      is_expected.to have_content('任務修改')
-      is_expected.to have_content('Title')
-      is_expected.to have_content('Content')
-      is_expected.to have_content('Started at')
-      is_expected.to have_content('Ended at')
+      find("a[href='/missions/#{find_mission(updated_mission)}']").click
     end
 
-    it 'has text_field' do
-      is_expected.to have_field(id: 'mission_title', with: "#{new_mission.title}")
-      is_expected.to have_field(id: 'mission_content', with: "#{new_mission.content}")
-      is_expected.to have_field(id: 'mission_started_at', with: "#{new_mission.started_at.strftime('%Y-%m-%dT%H:%M:%S')}")
-      is_expected.to have_field(id: 'mission_ended_at', with: "#{new_mission.ended_at.strftime('%Y-%m-%dT%H:%M:%S')}")
+    it 'deletes missions' do
+      is_expected.to have_content('任務列表')
+      is_expected.to have_content('新增任務')
+      is_expected.to have_no_content("#{updated_mission.title}")
+      is_expected.to have_no_content("#{updated_mission.content}")
     end
-
-    it 'has button' do
-      is_expected.to have_button('修改任務')
-    end
-  end  
+  end    
 end
+
