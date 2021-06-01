ac517b1961e96eaf326ca1ad63ac029080a8f1d1
dante <dantecherng@hotmail.com.tw>
2021-06-01 17:43:16 +0800

    Add feature spec for sorting (#14)

--- spec/support/features/shared_examples/mission.rb
+++ spec/support/features/shared_examples/mission.rb
@@ -4,6 +4,11 @@ shared_examples 'a missions list page' do
   it do
     is_expected.to have_content(I18n.t('missions.index.title'))
     is_expected.to have_content(I18n.t('missions.index.link.create'))
+    is_expected.to have_content('升冪')
+    is_expected.to have_content('降冪')
+    is_expected.to have_content('建立時間')
+    is_expected.to have_content('結束時間')
+    is_expected.to have_content('排序')
   end
 end
 
@@ -15,5 +20,21 @@ shared_examples 'a page that shows the missions' do
     is_expected.to have_content(mission.content)
     is_expected.to have_content(mission.started_at.strftime('%Y/%m/%d %H:%M'))
     is_expected.to have_content(mission.ended_at.strftime('%Y/%m/%d %H:%M'))
+  end
+end
+
+shared_examples 'a page that shows the missions sort by desc' do
+  it 'shows the mission' do
+    is_expected.to have_selector('ul > li:nth-child(1)', text: third_mission.title)
+    is_expected.to have_selector('ul > li:nth-child(2)', text: second_mission.title)
+    is_expected.to have_selector('ul > li:nth-child(3)', text: first_mission.title)
+  end
+end
+
+shared_examples 'a page that shows the missions sort by asc' do
+  it 'shows the mission' do
+    is_expected.to have_selector('ul > li:nth-child(1)', text: first_mission.title)
+    is_expected.to have_selector('ul > li:nth-child(2)', text: second_mission.title)
+    is_expected.to have_selector('ul > li:nth-child(3)', text: third_mission.title)
   end
 end
