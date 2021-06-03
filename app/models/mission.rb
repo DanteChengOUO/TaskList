14f64bf1bd719ed4e610a9c05d41872d08abeae6
dante <dantecherng@hotmail.com.tw>
2021-06-04 00:36:59 +0800

    Add validation for model (#15)

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class Mission < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true
+  validate :validate_end_time_after_start_time
+
+  private
+
+  def validate_end_time_after_start_time
+    return if started_at.blank? || ended_at.blank?
+    return if started_at < ended_at
+
+    error_message = I18n.t('activerecord.errors.models.mission.attributes.ended_at.before_start_time')
+    errors.add(:ended_at, error_message)
+  end
 end
