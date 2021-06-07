bd194107d34f9c22125f580b3c8a98feb7405d64
dante <dantecherng@hotmail.com.tw>
2021-06-07 21:42:42 +0800

    Add model spec for mission (#15)

---
+++ spec/models/mission_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Mission, type: :model do
+  let(:title_blank_error) { I18n.t('activerecord.errors.models.mission.attributes.title.blank') }
+  let(:title_too_long_error) { I18n.t('activerecord.errors.models.mission.attributes.title.too_long') }
+  let(:content_blank_error) { I18n.t('activerecord.errors.models.mission.attributes.content.blank') }
+
+  it { is_expected.to validate_presence_of(:title).with_message(title_blank_error) }
+  it { is_expected.to validate_length_of(:title).is_at_most(50).with_message(title_too_long_error) }
+  it { is_expected.to validate_presence_of(:content).with_message(content_blank_error) }
+
+  describe '#ended_at validation' do
+    context 'When #ended_at is bigger than #started_at' do
+      subject(:mission) { build(:mission) }
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'When #ended_at is less than #started_at' do
+      subject(:mission) { build(:mission, started_at: DateTime.now + 1, ended_at: DateTime.now) }
+      let(:error_message) { I18n.t('activerecord.errors.models.mission.attributes.ended_at.before_start_time') }
+
+      before { mission.save }
+
+      it { is_expected.to be_invalid }
+      it { expect(mission.errors.messages[:ended_at]).to include error_message }
+    end
+  end
+end
