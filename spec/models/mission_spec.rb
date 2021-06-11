dc47c9d64bb3d99a1b56683d54e28bb8ecc44e06
dante <dantecherng@hotmail.com.tw>
2021-06-12 01:05:04 +0800

    Add model spec for priority (#34)

--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe Mission, type: :model do
   let(:title_too_long_error) { I18n.t('activerecord.errors.models.mission.attributes.title.too_long') }
   let(:content_blank_error) { I18n.t('activerecord.errors.models.mission.attributes.content.blank') }
   let(:status_blank_error) { I18n.t('activerecord.errors.models.mission.attributes.status.blank') }
+  let(:priority_blank_error) { I18n.t('activerecord.errors.models.mission.attributes.priority.blank') }
 
   it { is_expected.to validate_presence_of(:title).with_message(title_blank_error) }
   it { is_expected.to validate_length_of(:title).is_at_most(50).with_message(title_too_long_error) }
   it { is_expected.to validate_presence_of(:content).with_message(content_blank_error) }
   it { is_expected.to validate_presence_of(:status).with_message(status_blank_error) }
+  it { is_expected.to validate_presence_of(:priority).with_message(priority_blank_error) }
 
   describe '#ended_at validation' do
     context 'When #ended_at is bigger than #started_at' do
