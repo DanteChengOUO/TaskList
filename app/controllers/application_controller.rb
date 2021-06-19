0ed1d392da11954083ffb50b3644b44a7e2d86c0
dante <dantecherng@hotmail.com.tw>
2021-06-20 00:11:14 +0800

    Fix path's redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_user!
-    redirect_to login_path, notice: t('missions.failure') unless session[:current_user_id]
+    redirect_to login_path, notice: t('missions.failure') if current_user.blank?
   end
 end
