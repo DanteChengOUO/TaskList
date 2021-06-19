f7af346bcd93c4b4756d4086eceaddcec18d289a
dante <dantecherng@hotmail.com.tw>
2021-06-19 23:44:13 +0800

    Add admin routes (#39)

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  # admin
+  namespace 'admin' do
+    resources :users, except: [:show]
+  end
 end
