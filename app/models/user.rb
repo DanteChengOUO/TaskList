84c34d47661a8dc49b1e68c483027d5439363c15
dante <dantecherng@hotmail.com.tw>
2021-06-22 23:28:08 +0800

    Add bcrypt for password encryption (#38)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_many :missions, dependent: :destroy
+  has_secure_password
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
