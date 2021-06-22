f796d86ec2ad5c34397982a5868233722b6f4841
dante <dantecherng@hotmail.com.tw>
2021-06-22 23:51:02 +0800

    Add password to migration for column is required

--- db/migrate/20210615091947_add_user_id_to_missions.rb
+++ db/migrate/20210615091947_add_user_id_to_missions.rb
@@ -3,8 +3,13 @@ class AddUserIdToMissions < ActiveRecord::Migration[6.0]
     add_column :missions, :user_id, :bigint
     add_index :missions, :user_id
 
-    admin = User.first_or_create!(name:'admin', email: 'admin@task.list')
-    Mission.update(user: admin)
+    if Mission.all.present?
+      user = User.first_or_create!(name:'user',
+                                   email: 'user@task.list',
+                                   password: '123123',
+                                   password_confirmation: '123123')
+      Mission.update(user: user)
+    end
 
     change_column_null :missions, :user_id, false
   end
